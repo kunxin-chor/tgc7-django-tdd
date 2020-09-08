f208bc6411eb49669399e3be59b5c5c138ce3ea2
kunxin-chor <chorkunxin@yahoo.com>
2020-09-08 06:20:26 +0000

    Added Vet model

--- animal/models.py
+++ animal/models.py
@@ -9,3 +9,11 @@ class Animal(models.Model):
     is_sterlized = models.BooleanField(blank=False, default=False)
     age = models.IntegerField(blank=False, default=1)
     gender = models.CharField(blank=False, max_length=1)
+
+
+class Vet(models.Model):
+    first_name = models.CharField(blank=False, max_length=255)
+    last_name = models.CharField(blank=False, max_length=255)
+    address = models.CharField(blank=False, max_length=255)
+    years = models.IntegerField(blank=False, default=1)
+    license = models.CharField(blank=False, max_length=100)
