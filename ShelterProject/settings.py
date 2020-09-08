a0f545def4657c36ee0f7ede4667e259013ac93b
kunxin-chor <chorkunxin@yahoo.com>
2020-09-08 05:27:46 +0000

    Added dotenv to load secret key

--- ShelterProject/settings.py
+++ ShelterProject/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Tell Django to laod the .env file
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4ve!0v8uu=h3t-6gm#$@6a+#3c^z2msre^^tv*jp#q7(f9ri^f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
