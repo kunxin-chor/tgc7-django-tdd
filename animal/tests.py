3e316cdb95e74858600978ecb61360c225d87925
kunxin-chor <chorkunxin@yahoo.com>
2020-09-08 05:49:56 +0000

    Created test case for animal model

--- animal/tests.py
+++ animal/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Animal
 
 # Create your tests here.
+class AnimalModelTestCase(TestCase):
+
+    def test_create_animal_model(self):
+        a = Animal()
+        a.name = "Fluffy"
+        a.breed = "Golden Retriever"
+        a.is_sterlized = False
+        a.age = 8
+        a.gender = "M"
+
